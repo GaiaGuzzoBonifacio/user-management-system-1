83e4af5439e946db05e245e31e01ea0b835ef5ad
Your Name <you@example.com>
2021-05-03 11:52:16 +0200

    Elenco degli utenti ok

--- src/view/list_users_view.php
+++ src/view/list_users_view.php
@@ -13,8 +13,8 @@
     <?php foreach($model->readAll() as $user ){ ?>
         <tr>
         <td><?= $user->getUserId() ?></td>
-        <td>Roberto</td>
-        <td>Rossi</td>
+        <td><?= $user->getFirstName()?></td>
+        <td><?= $user->getLastName() ?></td>
         <td>
         <a href="#" class="btn btn-secondary">edit </a>
         <a href="delete_user.php?user_id=10" class="btn btn-danger">delete </a>
