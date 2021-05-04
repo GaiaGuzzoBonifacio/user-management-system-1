4f5ed8aded85ba2045f7a570b6771c08dded7427
Your Name <you@example.com>
2021-05-04 18:44:29 +0200

    last fix

--- src/view/add_user_view.php
+++ src/view/add_user_view.php
@@ -57,10 +57,11 @@
              <?php if(isset($userId)) { ?>
                <!-- invece quando sono in modifica di un utente -->
                <div class="form-group mt-4 p-4 border border-danger">
-               <label for="">
+               <label class="text-danger">
                   Questo campo è visibile motivi didattici in realtà dovrebbe essere un <b>input[type=hidden]</b> <br> 
-                  serve a inviare via post, <b>User::userId</b> dell'istanza dell'utente da aggiornare <br>
+                  serve a inviare via POST, il valore dello <b>userId</b> dell'istanza di User da aggiornare sul database<br>
                </label>
+               <label class="d-block text-bold">id dell'utente che sto modificando</label>
                <input type="text" name="userId" value="<?= $userId ?>" class="form-control">
              </div>
 
