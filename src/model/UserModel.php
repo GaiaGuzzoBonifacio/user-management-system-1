cbb0dddc8d1f212d5ee30be635489f32c327dd69
Your Name <you@example.com>
2021-05-04 18:50:04 +0200

    fix connessione

--- src/model/UserModel.php
+++ src/model/UserModel.php
@@ -12,7 +12,7 @@ class UserModel
     public function __construct()
     {
         try {
-            $this->conn = new PDO('mysql:dbname='.AppConfig::DB_NAME.';host='.AppConfig::DB_HOST, AppConfig::DB_USER, AppConfig::DB_PASSWORD);
+            $this->conn = new PDO('mysql:dbname=corso_formarete;host=localhost', 'root', '');
             $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             // TODO: togliere echo
