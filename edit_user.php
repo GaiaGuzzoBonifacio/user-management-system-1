614fb249a25dd463ad13d4d1d64922088bec45e1
Your Name <you@example.com>
2021-05-04 10:36:49 +0200

    ragionamenti vari su update

--- edit_user.php
+++ edit_user.php
@@ -1,23 +1,24 @@
-<?php 
-//require "autoload.php";
-//require __DIR__."/vendor/testTools/testTool.php";
+<?php
 
 use sarassoroberto\usm\entity\User;
 use sarassoroberto\usm\model\UserModel;
-use sarassoroberto\usm\validator\bootstrap\ValidationFormHelper;
 use sarassoroberto\usm\validator\UserValidation;
 
-require "./__autoload.php";
-
-$action = './add_user_form.php';
-// require __DIR__."/src/validator/fondation/ValidationFormHelper.php"
-// print_r($_POST);
+$action = "edit_user.php?user_id=";
 if($_SERVER['REQUEST_METHOD']==='GET'){
-    list($firstName,$firstNameClass,$firstNameClassMessage,$firstNameMessage) = ValidationFormHelper::getDefault();
-    
+    $user_id = filter_input(INPUT_GET,'user_id',FILTER_SANITIZE_NUMBER_INT);
+    $userModel = new UserModel();
+    $user = $userModel->readOne($user_id);
+
+    $firstName = $user->getFirstName();
+    $firstNameClass = '';   
+    $lastName = $user->getLastName();    
+    $email = $user->getEmail();    
 }
 
 if ($_SERVER['REQUEST_METHOD']==='POST') {
+    // passare  id in qualche modo
+    // - hidden field  input type hidden
     $user = new User($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['birthday']);
     $val = new UserValidation($user);
     $firstNameValidation = $val->getError('firstName');
@@ -31,6 +32,3 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
        // header('location: ./list_users.php');
     }
 }
-
-include 'src/view/add_user_view.php';
-?>
