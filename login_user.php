58bac019dcc64ddbb6dd500584f84b38983c14d9
Your Name <you@example.com>
2021-05-19 13:02:40 +0200

    redirect e log out

--- login_user.php
+++ login_user.php
@@ -1,6 +1,8 @@
 <?php
 
 use sarassoroberto\usm\model\UserModel;
+use sarassoroberto\usm\service\UserSession;
+
 require "./__autoload.php";
 
 
@@ -8,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD']==='POST') {
     $email = filter_input(INPUT_POST,'email',FILTER_SANITIZE_STRING);
     $password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
     if($email && $password){
-        $user = (new UserModel())->autenticate($email,$password);
+        $user = (new UserSession())->autenticate($email,$password);
         
         if(is_null($user)){ $msg = "credenziali errate"; }else{
             header("location: list_users.php");
