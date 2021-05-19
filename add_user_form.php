6d08548dc3eb8962ec1aa1a78ba28048f596c6e6
Your Name <you@example.com>
2021-05-19 06:01:33 +0200

    elenco con mail

--- add_user_form.php
+++ add_user_form.php
@@ -1,5 +1,6 @@
 <?php 
 use sarassoroberto\usm\entity\User;
+use sarassoroberto\usm\factory\UserFactory;
 use sarassoroberto\usm\model\UserModel;
 use sarassoroberto\usm\validator\bootstrap\ValidationFormHelper;
 use sarassoroberto\usm\validator\UserValidation;
@@ -18,25 +19,29 @@ if($_SERVER['REQUEST_METHOD']==='GET'){
     list($email,$emailClass,$emailClassMessage,$emailMessage) = ValidationFormHelper::getDefault();
     list($birthday,$birthdayClass,$birthdayClassMessage,$birthdayMessage) = ValidationFormHelper::getDefault();    
     list($birthday,$birthdayClass,$birthdayClassMessage,$birthdayMessage) = ValidationFormHelper::getDefault();    
+    list($password,$passwordClass,$passwordClassMessage,$passwordMessage) = ValidationFormHelper::getDefault();    
 }
 
 if ($_SERVER['REQUEST_METHOD']==='POST') {
 
-    $user = new User($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['birthday']);
+    $user = UserFactory::fromArray($_POST);
     $val = new UserValidation($user);
     $firstNameValidation = $val->getError('firstName');
     $lastNameValidation = $val->getError('lastName');
     $emailValidation = $val->getError('email');
     $birthdayValidation = $val->getError('birthday');
+    $passwordValidation = $val->getError('password');
 
     list($firstName, $firstNameClass, $firstNameClassMessage, $firstNameMessage) = ValidationFormHelper::getValidationClass($firstNameValidation);
     list($lastName, $lastNameClass, $lastNameClassMessage, $lastNameMessage) = ValidationFormHelper::getValidationClass($lastNameValidation);
     list($email, $emailClass, $emailClassMessage, $emailMessage) = ValidationFormHelper::getValidationClass($emailValidation);
     list($birthday, $birthdayClass, $birthdayClassMessage, $birthdayMessage) = ValidationFormHelper::getValidationClass($birthdayValidation);
+    list($password,$passwordClass,$passwordClassMessage,$passwordMessage) = ValidationFormHelper::getValidationClass($passwordValidation);    
 
     $user->setBirthday($birthday);
 
     if ($val->getIsValid()) {
+        
         // TODO
         $userModel = new UserModel();
         $userModel->create($user);
